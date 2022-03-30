1a01e2b39e37efe0f5dcb464b81bc4eb5cd92eb8
rxdvd <rxdvd0@gmail.com>
2022-03-30 11:16:18 +0100

    implement / routes

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
 from flask import Flask, render_template, request, jsonify, redirect
 from werkzeug import exceptions
+import random
+
 from db_config import get_collection
 
 app = Flask(__name__)
 
-@app.route('/')
+urls = []
+
+def generate_short_url():
+    possible_chars = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    short_url = None
+
+    # check for collisions
+    while not short_url or next((url for url in urls if url['short'] == short_url), None):
+        short_url = ''.join([random.choice(possible_chars) for _ in range(5)])
+    
+    return short_url
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    if request.method == 'POST':
+        long_url = request.form['url']
+        short_url = next((url['short'] for url in urls if url['long'] == long_url), None)
+        if not short_url:
+            short_url = generate_short_url()
+            urls.append({ 'long': long_url, 'short': short_url })
+        return render_template('result.html', long_url=long_url, short_url=short_url, title='Your shortened URL')
+    else:
+        return render_template('home.html', title='Make a short URL!')
+
+if __name__ == '__main__':
+    app.run()
