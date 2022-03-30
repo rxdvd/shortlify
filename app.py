9b6e9c00c1509c392cfbafbcead920d0d1cc1e1b
rxdvd <rxdvd0@gmail.com>
2022-03-30 11:30:45 +0100

    add error handlers for 400, 404, 405, 500

--- app.py
+++ app.py
@@ -38,5 +38,21 @@ def redirect_to(id):
     else:
         return redirect('/')
 
+@app.errorhandler(exceptions.BadRequest)
+def handle_400(err):
+    return render_template('error.html', error=err, code=400, title='Bad Request')
+
+@app.errorhandler(exceptions.NotFound)
+def handle_404(err):
+    return render_template('error.html', error=err, code=404, title='Not Found')
+
+@app.errorhandler(exceptions.MethodNotAllowed)
+def handle_405(err):
+    return render_template('error.html', error=err, code=405, title='Method Not Allowed')
+
+@app.errorhandler(exceptions.InternalServerError)
+def handle_500(err):
+    return render_template('error.html', error=err, code=500, title='Internal Server Error')
+
 if __name__ == '__main__':
     app.run()
