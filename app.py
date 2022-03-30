7551757a96d72dc2fc89a0847bae28086ccac41a
rxdvd <rxdvd0@gmail.com>
2022-03-30 16:53:24 +0100

    add CORS wrapper to app

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request
+from flask_cors import CORS
 from werkzeug import exceptions
 
 from controllers import urls
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
