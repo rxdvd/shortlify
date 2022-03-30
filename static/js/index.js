e3dcc8f60d6f9cfd17a2cd956cd848cba6f71242
k3ith99 <hamza_saleh1999@hotmail.com>
2022-03-30 17:01:26 +0100

    css fine

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,5 @@
 let copyText = document.getElementById("new_url")
-let copy_link = document.getElementById("copy_link")
-function copyLink(){
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(copyText.value);
 
-  /* Alert the copied text */
-  alert("Copied the text: " + copyText.value)
+function myFunction() {
+  navigator.clipboard.writeText(copyText.value);
 }
-copy_link.addEventListener("click", copyLink())
